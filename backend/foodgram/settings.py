627de609942d3dea81223eb654002c184118edfe
progqwer <ilyatsygancov@gmail.com>
2023-05-05 18:22:50 +0000

    commit2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='acej2l)w5t8dyl+vaep9#ywqja3$ai7(!9
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-# Application definition
+# Application definitin
 
 INSTALLED_APPS = [
     'colorfield',
