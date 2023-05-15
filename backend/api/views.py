c0a5dfb5d478906b018379e94c612255cb020cd1
Progqwer <ilyatsygancov@gmail.com>
2023-05-15 14:28:15 +0300

    commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -33,6 +33,7 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = (IsAdminOrReadOnly,)
+    pagination_class = None
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
